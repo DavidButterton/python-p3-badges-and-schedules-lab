2f7222d67b953d29a6dbf819c126e4a93d24984e
DavidButterton <sadhu.butterton@gmail.com>
2023-12-13 10:55:29 -0800

    submission

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,30 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
+
+# print(badge_maker("tracy"))
 
 def batch_badge_creator(names):
-    return None
+    return [badge_maker(name) for name in names]
+   
+# print(batch_badge_creator(["alan"]))
 
 def assign_rooms(names):
-    return None
+    room_assignments = []
+    for index, name in enumerate(names, start=1):
+        room_assignments.append(f"Hello, {name}! You'll be assigned to room {index}!")
+    return room_assignments
+        
+# print(assign_rooms(["luna"]))
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    room_assignments = assign_rooms(names)
+    
+    for item in badges:
+        print(item)
+    for item in room_assignments:
+        print(item)
+    
+    
+
+print(printer(["hana"]))
